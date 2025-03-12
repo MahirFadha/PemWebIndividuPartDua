27469a298fa09fa5c2152676cc14e9e32b6ad91f
Mahir Fadha <mahirfadha@gmail.com>
2025-03-13 00:34:45 +0700

    HTML & CSS Login Thread

---
+++ Web/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman HTML dalam PHP</title>
+    <link rel="stylesheet" href="tampilan.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="kiri">
+            <img class="gambarKiri" src="Image/fotoKiri.png" alt="Dekorasi Kiri">
+        </div>
+        <div class="tengah">
+            <img class="gambarTengah" src="Image/fotoTengah.png" alt="DekorasiTengah">
+            <h3 class="header">Login with your Instagram Account</h3>
+            <div class="input">
+                <input class="inputUsername" type="text" value="Username">
+                <input class="inputPw" type="text" value="Password">
+                <button class="btnLogin">Login</button>
+            </div>
+            <h5 class="lupasandi">Lupa kata sandi?</h5>
+            <h5 class="atau">── atau ──</h5>
+            <div class="Continue">
+                <button class="btnContinue"><img class="iconIg" src="Image/iconIg.png" alt="iconIg">Continue with Instagram</button>
+            </div>
+        </div>
+        <div class="kanan">
+            <img class="gambarKanan" src="Image/fotoKanan.png" alt="Dekorasi Kanan">
+        </div>
+    </div>
+</body>
+
+</html>
