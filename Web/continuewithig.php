3fc79c8df61d25567343a0e0fa267556db2db6ce
Mahir Fadha <mahirfadha@gmail.com>
2025-03-16 04:43:50 +0700

    Change Page

--- Web/continuewithig.php
+++ Web/continuewithig.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Instagram</title>
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
 </head>
 <style>
@@ -151,7 +151,7 @@
     </div>
     </div>
     <script>
-        var btnBack = document.getElementById(".back")
+        var btnBack = document.querySelector(".back")
         btnBack.onclick = function() {
             window.location.href = "index.php"
         }
